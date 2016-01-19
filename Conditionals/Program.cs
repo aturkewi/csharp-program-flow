e207db38ed9f7913e1c8f4c1435b09d1773db89d
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-19 10:03:38 -0500

    Prints happy birthdays

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -6,17 +6,27 @@ namespace Conditionals
 	{
 		public static void Main (string[] args)
 		{
-			Console.Write ("What is your age?");
-			string ageAsString = Console.ReadLine();
-			int age = int.Parse (ageAsString);
+			// Console.Write ("What is your age?");
+			// string ageAsString = Console.ReadLine();
+			// int age = int.Parse (ageAsString);
+			// 
+			// if (age > 65) {
+			// 	Console.WriteLine ("You are a senior citizen");
+			// }else if (age >= 21){
+			// 	Console.WriteLine ("You are an adult");
+			// }else{
+			// 	Console.WriteLine ("You are not yet a legal adult");
+			// }
 
-			if (age > 65) {
-				Console.WriteLine ("You are a senior citizen");
-			}else if (age >= 21){
-				Console.WriteLine ("You are an adult");
-			}else{
-				Console.WriteLine ("You are not yet a legal adult");
+			Console.Write("How old are you? ");
+			int age = int.Parse (Console.ReadLine ());
+
+			while (age > 0) {
+				Console.Write ("Happy Birthday ");
+				age--;
 			}
+
+			Console.WriteLine ("\nTaDa!!");
 		}
 	}
 }
