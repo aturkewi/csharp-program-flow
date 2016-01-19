9473cf0776ebc83465debd992bc19fefaa41537e
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-01-19 09:59:26 -0500

    Get age and returns string based on condition

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -6,7 +6,18 @@ namespace Conditionals
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.Write ("What is your age?");
+			string ageAsString = Console.ReadLine();
+			int age = int.Parse (ageAsString);
+
+			if (age > 65) {
+				Console.WriteLine ("You are a senior citizen");
+			}else if (age >= 21){
+				Console.WriteLine ("You are an adult");
+			}else{
+				Console.WriteLine ("You are not yet a legal adult");
+			}
 		}
 	}
 }
+	
